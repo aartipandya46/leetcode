625b72b7166f5a89f48cd900c75a697a3057d47e
aartipandya46 <64550144+aartipandya46@users.noreply.github.com>
2023-01-14 19:50:37 +0530

    Find Triangular Sum of an Array
    You are given a 0-indexed integer array nums, where nums[i] is a digit between 0 and 9 (inclusive).
    
    The triangular sum of nums is the value of the only element present in nums after the following process terminates:
    
    Let nums comprise of n elements. If n == 1, end the process. Otherwise, create a new 0-indexed integer array newNums of length n - 1.
    For each index i, where 0 <= i < n - 1, assign the value of newNums[i] as (nums[i] + nums[i+1]) % 10, where % denotes modulo operator.
    Replace the array nums with newNums.
    Repeat the entire process starting from step 1.
    Return the triangular sum of nums.

---
+++ TriangularSumOfArray.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var triangularSum = function(nums) {
+    while(nums.length>1){
+        const result=[];
+        for(let i=0;i<nums.length-1;i++){
+            result.push((nums[i]+nums[i+1])%10);
+        }
+        nums.length=0;
+        nums=result;
+    }
+    return nums[0];
+};
