e592ca8d610fd5a535174838494cdafc97e92ac3
aartipandya46 <64550144+aartipandya46@users.noreply.github.com>
2023-03-05 14:57:35 +0530

    Create isSubsequence.js

---
+++ isSubsequence.js
@@ -0,0 +1,9 @@
+var isSubsequence = function(s, t) {
+    for(let i=0;i<s.length;i++){
+        const index=t.indexOf(s[i]);
+        if(index===-1)
+            return false;
+        t=t.slice(index+1);
+    }
+    return true;
+};
